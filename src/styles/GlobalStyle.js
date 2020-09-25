0ef08d25ee27bfd202ca9bba8825fe82c6cf13cf
Fabio Ribeiro <fabiohkd@gmail.com>
2020-09-25 13:33:26 -0300

    Adicionado react-helmet

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { createGlobalStyle } from "styled-components";
 import { breakAt, BreakpointSize } from "./Breakpoints";
+import { Helmet } from "react-helmet";
 
 const GlobalStyle = createGlobalStyle`
 html{
@@ -71,4 +73,16 @@ h6{
 
 `;
 
-export default GlobalStyle;
+const GlobalStyleComposed = () => (
+  <>
+    <GlobalStyle />
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  </>
+);
+
+export default GlobalStyleComposed;
